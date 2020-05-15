9ff51076fe045324cfa25e20c088803833589497
ricardo.ostos <ricardo.ostos@gmail.com>
2020-05-15 10:28:40 -0500

    Ajustando los archivos.

---
+++ ASP Net Core Vuejs/Sistema/Sistema.Entidades/Almacen/Articulo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Articulo
+    {
+        [Key]
+        public int idarticulo { get; set; }
+
+        [Required]
+        public int idcategoria { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de contener más de 50 caracteres, ni menos de 3 caracteres.")]
+        public string codigo { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre no debe de contener más de 100 caracteres, ni menos de 3 caracteres.")]
+        public string nombre { get; set; }
+
+        [Required]
+        public decimal precio_venta { get; set; }
+
+        [Required]
+        public int stock { get; set; }
+
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+
+        public Categoria categoria { get; set; }
+
+    }
+}
